4d9f5bf94798341f3074777a7712f2e4b18fdc14
Lucas Souza <lucasnando800@hotmail.com>
2025-08-31 21:56:05 -0300

    Remove VSCode settings and add GitHub Actions CI workflow for Pulumi project

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("primeiro-bucket", {
+  bucket: 'primeiro-bucket-pulumi-pos-rocketseat',
+  tags: {
+    IAC: "true"
+  }
+});
 
-// Export the name of the bucket
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+  name: "primeiro-ecr",
+  imageTagMutability: "IMMUTABLE",
+  tags: {
+    IAC: "true"
+  }
+})
+
 export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
